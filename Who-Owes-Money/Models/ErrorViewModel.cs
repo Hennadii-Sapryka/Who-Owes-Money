e2b1ff75751f5669455467183bc6636e95bd5220
Hennadii Sapryka <paperoncini@gmail.com>
2021-04-26 20:17:30 +0300

    Add project files.

---
+++ Who-Owes-Money/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Who_Owes_Money.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
