e0ca01aaeb1c35be9027f6d1f2a1ffae0c62676c
Hennadii Sapryka <paperoncini@gmail.com>
2021-04-27 13:59:58 +0300

    Added CalcullationController (business logic), class ResultCalculation. Implement CRUD principles. Added Views for controllers.

---
+++ Who-Owes-Money/Controllers/CalculationController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Who_Owes_Money.Data;
+using Who_Owes_Money.Models;
+
+namespace Who_Owes_Money.Controllers
+{
+    public class CalculationController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userContext;
+
+        public CalculationController(ApplicationDbContext context, UserManager<IdentityUser> userContext)
+        {
+            _context = context;
+            _userContext = userContext;
+        }
+
+        public IActionResult Calculation()
+        {
+            var products = _context.Product.ToList();
+
+            if (products.FirstOrDefault() == null)
+            {
+                return View("NotFound");
+            }
+
+            int average = products
+                .Sum(m => m.Price)
+                / products
+                .GroupBy(m => m.UserName)
+                .Count();
+
+            List<ResultCalculation> results = new List<ResultCalculation>();
+
+            Product[] people = products
+                .GroupBy(m => m.UserName)
+                .Select(g => new Product
+                {
+                    UserName = g.Key,
+                    Price = g.Sum(p => p.Price)
+                }).ToArray();
+
+            Product[] paidMoreMoney = people.Where(m => m.Price > average).ToArray();
+            Product[] paidLessMoney = people.Where(m => m.Price < average).ToArray();
+
+            for (int i = 0; i < paidMoreMoney.Length; i++)
+            {
+                for (int j = 0; j < paidLessMoney.Length; j++)
+                {
+                    int count = 0;
+                    while (paidLessMoney[j].Price != average)
+                    {
+                        if (paidMoreMoney[i].Price != average)
+                        {
+                            paidMoreMoney[i].Price -= 1;
+                            paidLessMoney[j].Price += 1;
+                            count++;
+                        }
+                        if (paidLessMoney[j].Price == average)
+                        {
+                            results.Add(new ResultCalculation
+                            {
+                                UserLess = paidLessMoney[j].UserName,
+                                UserMore = paidMoreMoney[i].UserName,
+                                Money = count,
+                                Average = average,
+                            });
+                        }
+                        else if (paidMoreMoney[i].Price == average)
+                        {
+                            results.Add(new ResultCalculation
+                            {
+                                UserLess = paidLessMoney[j].UserName,
+                                UserMore = paidMoreMoney[i].UserName,
+                                Money = count
+                            });
+                            i++;
+                            count = 0;
+                        }
+                    }
+                }
+            }
+
+            return View(results);
+        }
+    }
+}
---
+++ Who-Owes-Money/Models/ResultCalculation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Who_Owes_Money.Models
+{
+     public class ResultCalculation
+    {
+        public string UserLess { get; set; }
+        public string UserMore { get; set; }
+        public int Money { get; set; }
+        public int Average { get; set; }
+    }
+}
